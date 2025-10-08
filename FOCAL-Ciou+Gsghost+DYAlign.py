4f0d5473376d5a95eb2b778447a37e8bd6832e7d
jqk-123 <1036103149@qq.com>
2025-10-08 22:16:12 +0800

    Update and rename FOCAL-Ciou+Gs+ns_ghost+dy_ducalcu.py to FOCAL-Ciou+Gsghost+DYAlign.py

--- FOCAL-Ciou+Gsghost+DYAlign.py
+++ FOCAL-Ciou+Gsghost+DYAlign.py
@@ -6,17 +6,18 @@ script_name = os.path.splitext(os.path.basename(__file__))[0]
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO(r'/home/yw/data/lxs/xiugai/35gs+dy_youhua/yolo11s-GSconv+ns_ghostconv+dy_ducalcu.yaml')
+    model = YOLO(r'yolo11s-GSconv+ns_ghostconv+dy_ducalcu.yaml')
 
     # Train the model
     train_results = model.train(
-        data=r"/home/yw/data/lxs/xiugai/1dataset/SIMD3junheng.yaml",  # path to dataset YAML
+        data=r"SIMD3junheng.yaml",  # path to dataset YAML
         epochs=200,  # number of training epochs
         imgsz=640,  # training image size
         device="0",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
         batch=8,
         patience=50,
         pretrained=False,
-        project='/home/yw/data/lxs/xiugai/runs/dy+gs_xiugai',
+        project='runs/dy+gs_xiugai',
         name=f'{script_name}'  # 使用运行文件的名称作为目录名
     )
+
