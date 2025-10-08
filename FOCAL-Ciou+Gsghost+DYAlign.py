74816bd8d36566845e9924450af58fae29fc89cd
jqk-123 <1036103149@qq.com>
2025-10-08 22:16:54 +0800

    Update FOCAL-Ciou+Gsghost+DYAlign.py

--- FOCAL-Ciou+Gsghost+DYAlign.py
+++ FOCAL-Ciou+Gsghost+DYAlign.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 import os
 
-# 获取当前运行文件的名称（不包括扩展名）
+
 script_name = os.path.splitext(os.path.basename(__file__))[0]
 
 if __name__ == '__main__':
@@ -10,14 +10,15 @@ if __name__ == '__main__':
 
     # Train the model
     train_results = model.train(
-        data=r"SIMD3junheng.yaml",  # path to dataset YAML
+        data=r"SIMD.yaml",  # path to dataset YAML
         epochs=200,  # number of training epochs
         imgsz=640,  # training image size
         device="0",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
         batch=8,
         patience=50,
         pretrained=False,
-        project='runs/dy+gs_xiugai',
-        name=f'{script_name}'  # 使用运行文件的名称作为目录名
+        project='runs/FOCAL-Ciou+Gsghost+DYAlign',
+        name=f'{script_name}'  
     )
+
 
